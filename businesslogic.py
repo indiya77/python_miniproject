2982651892135f9e1c0095f412fc90d6cdeb06ee
indiya77 <indiyabhenly@gmail.com>
2021-07-15 21:31:00 +0530

    test cases added

---
+++ businesslogic.py
@@ -0,0 +1,23 @@
+def kg2g(kg):
+    return float(kg)*1000
+
+def kg2p(kg):
+    return float(kg)*2.205
+
+def kg2o(kg):
+    return float(kg)*35.274
+
+def m2cm(m):
+    return float(m)*100
+
+def m2f(m):
+    return float(m)*3.281
+
+def m2i(m):
+    return float(m)*39.37
+
+def c2f(c):
+    return float(c)*9/5+32
+
+def c2k(c):
+    return float(c)+273.15
